7075db83aa36d5582f1448a75c53861b07aa4f3c
Soyeon Kim <s2023@e-mirim.hs.kr>
2022-04-27 09:56:46 +0900

    Add db update, delete code in "SpringJDBC" DB 데이터 수정 및 삭제 코드 추가

--- SpringJDBC/src/main/java/kr/hs/study/dao/TestDAO.java
+++ SpringJDBC/src/main/java/kr/hs/study/dao/TestDAO.java
@@ -19,4 +19,17 @@ public class TestDAO {
 		String sql = "insert into test values(?, ?)";
 		db.update(sql, bean.getData1(), bean.getData2());
 	}
+	
+	//update
+	public void update_data(TestBean bean) {
+		String sql = "update test set data2=? where data1=?";
+		//data1이 100인 data2값을 testtest로 변경
+		db.update(sql, bean.getData2(), bean.getData1());
+	}
+	
+	//delete
+	public void delete_data(int data1) {
+		String sql = "delete from test where data1=?";
+		db.update(sql, data1);
+	}
 }
--- SpringJDBC/src/main/java/kr/hs/study/main/MainClass.java
+++ SpringJDBC/src/main/java/kr/hs/study/main/MainClass.java
@@ -13,19 +13,33 @@ public class MainClass {
 		
 		TestDAO dao = ctx.getBean(TestDAO.class);
 		
-		TestBean bean1 = new TestBean();
-		bean1.setData1(100);
-		bean1.setData2("spring100");
+//		TestBean bean1 = new TestBean();
+//		bean1.setData1(100);
+//		bean1.setData2("spring100");
+//		TestBean bean2 = new TestBean();
+//		bean2.setData1(200);
+//		bean2.setData2("spring200");
 		
-		TestBean bean2 = new TestBean();
-		bean2.setData1(200);
-		bean2.setData2("spring200");
+//		dao.insert_data(bean1);
+//		dao.insert_data(bean2);
+//		System.out.println("Inserted!");
 		
-		dao.insert_data(bean1);
-		dao.insert_data(bean2);
 		
-		System.out.println("Inserted!");
+//		TestBean bean3 = new TestBean();
+//		bean3.setData1(100);
+//		bean3.setData2("testtest");
+//		
+//		dao.update_data(bean3);
+//		System.out.println("Updated!");
 		
+		
+		TestBean bean4 = new TestBean();
+		bean4.setData1(200);
+		bean4.setData2("testtest");
+		
+		dao.delete_data(bean4.getData1());
+		System.out.println("Deleted!");
+
 		ctx.close();
 	}
 
